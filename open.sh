c3491e0d3a2ee7e0fd0eb82d20e6d2ea5f1f4199
Milos Vasic <i@mvasic.ru>
2023-09-09 18:01:51 +0300

    Caleanup.

--- open.sh
+++ open.sh
@@ -16,8 +16,49 @@ PROGRAM="$1"
 PROJECT="$2"
 HERE="$(pwd)"
 PROGRAM_VSCODE="code"
-SCRIPT_INSTALL_VSCODE="$HERE/Toolkit/Utils/VSCode/install.sh"
-SCRIPT_GET_PROGRAM="$HERE/Toolkit/Utils/Sys/Programs/get_program.sh"
+DIR_HOME=$(eval echo ~"$USER")
+FILE_ZSH_RC="$DIR_HOME/.zshrc"
+FILE_BASH_RC="$DIR_HOME/.bashrc"
+
+FILE_RC=""
+    
+if test -e "$FILE_ZSH_RC"; then
+
+  FILE_RC="$FILE_ZSH_RC"
+
+else
+
+    if test -e "$FILE_BASH_RC"; then
+
+      FILE_RC="$FILE_BASH_RC"
+
+    else
+
+      echo "ERROR: No '$FILE_ZSH_RC' or '$FILE_BASH_RC' found on the system"
+      exit 1
+    fi
+fi
+
+# shellcheck disable=SC1090
+. "$FILE_RC" >/dev/null 2>&1
+
+if [ -z "$SUBMODULES_HOME" ]; then
+
+  echo "ERROR: The SUBMODULES_HOME is not defined"
+  exit 1
+fi
+
+DIR_TOOLKIT="$SUBMODULES_HOME/Software-Toolkit"
+
+if ! test -e "$DIR_TOOLKIT"; then
+
+  echo "ERROR: Toolkit directory not found '$DIR_TOOLKIT'"
+  exit 1
+fi
+
+SCRIPT_INSTALL_VSCODE="$DIR_TOOLKIT/Utils/VSCode/install.sh"
+SCRIPT_GET_PROGRAM="$DIR_TOOLKIT/Utils/Sys/Programs/get_program.sh"
+
 VSCODE_INSTALLATION_PARAMS="$HERE/Recipes/vscode_installation_params.sh"
 
 if ! test -e "$SCRIPT_GET_PROGRAM"; then
