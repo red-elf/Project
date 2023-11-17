acb20771f87922e7e4a763a593f81732ecad2050
Milos Vasic <i@mvasic.ru>
2023-11-17 14:58:22 +0300

    "Small fixes."

--- prepare.sh
+++ prepare.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
+DIR_HOME=$(eval echo ~"$USER")
+FILE_ZSH_RC="$DIR_HOME/.zshrc"
+FILE_BASH_RC="$DIR_HOME/.bashrc"
+
+FILE_RC=""
+    
+if test -e "$FILE_ZSH_RC"; then
+
+  FILE_RC="$FILE_ZSH_RC"
+
+else
+
+    if test -e "$FILE_BASH_RC"; then
+
+      FILE_RC="$FILE_BASH_RC"
+
+    else
+
+      echo "ERROR: No '$FILE_ZSH_RC' or '$FILE_BASH_RC' found on the system"
+      exit 1
+    fi
+fi
+
 if [ -z "$SUBMODULES_HOME" ]; then
-
-  DIR_HOME=$(eval echo ~"$USER")
-  FILE_ZSH_RC="$DIR_HOME/.zshrc"
-  FILE_BASH_RC="$DIR_HOME/.bashrc"
-
-  FILE_RC=""
-      
-  if test -e "$FILE_ZSH_RC"; then
-
-    FILE_RC="$FILE_ZSH_RC"
-
-  else
-
-      if test -e "$FILE_BASH_RC"; then
-
-        FILE_RC="$FILE_BASH_RC"
-
-      else
-
-        echo "ERROR: No '$FILE_ZSH_RC' or '$FILE_BASH_RC' found on the system"
-        exit 1
-      fi
-  fi
 
   # shellcheck disable=SC2002
   if ! cat "$FILE_RC" | grep "SUBMODULES_HOME=" >/dev/null 2>&1; then
