d818e11f10eab5d5d96c10b6fd1e3f99b9d72278
Milos Vasic <i@mvasic.ru>
2023-12-02 19:09:12 +0300

    "SonarQube multi-project support."

--- open.sh
+++ open.sh
@@ -223,6 +223,18 @@ if sh "$SCRIPT_GET_PROGRAM" "$PROGRAM" >/dev/null 2>&1; then
 
   if [ "$PROGRAM" = "$PROGRAM_VSCODE" ]; then
 
+      SCRIPT_GET_SONAR_NAME="get_sonar_project_name.sh"
+      SCRIPT_GET_SONAR_NAME_FULL="$SUBMODULES_HOME/Software-Toolkit/Utils/SonarQube/$SCRIPT_GET_SONAR_NAME"
+
+      if ! test -e "$SCRIPT_GET_SONAR_NAME_FULL"; then
+
+          echo "ERROR: Script not found '$SCRIPT_GET_SONAR_NAME_FULL'"
+          exit 1
+      fi
+
+      # shellcheck disable=SC1090
+      . "$SCRIPT_GET_SONAR_NAME_FULL"
+
       if sh "$DIR_TOOLKIT/Utils/SonarQube/configure_sonar_lint.sh" >/dev/null 2>&1; then
 
         echo "SonarLint has been configured"
